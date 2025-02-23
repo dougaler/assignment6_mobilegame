643593789d13c430d0eae0583033301ad2151df3
Chris Dougall <dougaler@mail.uc.edu>
2025-02-22 22:31:58 -0500

    finished assignment

---
+++ assignment6/ContactsGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment6
+{
+	public class ContactsGroup : List<Contacts>
+	{
+		public string GroupName { get; set; }
+		public ContactsGroup(string groupName, List<Contacts> contact) : base(contact)
+		{
+			GroupName = groupName;
+		}
+	}
+}
---
+++ assignment6/obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("assignment6.MainApplication, assignment6, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64d99b448daed4058f.MainApplication.class, crc64d99b448daed4058f.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
